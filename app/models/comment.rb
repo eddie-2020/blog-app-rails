7f4d974e278432273c61b1718e701b28cde98a32
Edward <oluyaratosin123@gmail.com>
2022-05-02 16:38:32 +0100

    Blog app - processing data in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :users
   belongs_to :posts
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    posts.increment!(:update_comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :users
+  has_many :comments
+  has_many :likes
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
+    users.increment!(:update_posts_counter)
+  end
 end
