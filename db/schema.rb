c35d9897fd3c48446f0417aea6a685be83100306
Edward <oluyaratosin123@gmail.com>
2022-05-03 20:49:32 +0100

    Blog app - views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_210704) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_170451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_210704) do
     t.bigint "posts_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["users_id"], name: "index_comments_on_users_id"
   end
@@ -29,6 +33,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_210704) do
     t.bigint "posts_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["posts_id"], name: "index_likes_on_posts_id"
     t.index ["users_id"], name: "index_likes_on_users_id"
   end
@@ -41,6 +49,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_210704) do
     t.bigint "users_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
@@ -53,9 +63,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_210704) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "posts", column: "posts_id"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "likes", "users", column: "users_id"
+  add_foreign_key "posts", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "users_id"
 end
